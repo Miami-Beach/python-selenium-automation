57ec8ae818b853e80524cd3ed41471364f8aecee
Vlad <vlad@MacBook.local>
2019-10-07 20:58:24 -0400

    HW2

---
+++ features/Locators.py
@@ -0,0 +1,18 @@
+# Amazon Logo
+"//i[@class='a-icon a-icon-logo']"
+# Create Amazon Account
+"//a[@id='createAccountSubmit']"
+# Email field
+"//input[@type='email']"
+# Continue button
+"//input[@id='continue']"
+# Need help
+"//span[@class='a-expander-prompt']"
+# Forgot your password
+"//a[@id='auth-fpp-link-bottom']"
+# Other issues with sign in
+"//a[@id='ap-other-signin-issues-link']"
+# Conditions of use
+"//a[contains(@href, 'conditions_of_use')]"
+# Privacy notice
+"//a[contains(@href, 'privacy_notice')]"
